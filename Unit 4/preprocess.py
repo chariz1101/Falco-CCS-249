6fb189fa4d25350e76e37224c09320d660bdd5f2
Chariz Dianne Falco <126627685+chariz1101@users.noreply.github.com>
2025-04-27 21:47:02 +0800

    Delete Unit 4 directory

--- Unit 4/preprocess.py
+++
@@ -1,8 +0,0 @@
-# generated from ChatGPT 
-
-import re
-
-def preprocess(text):
-    text = text.lower()  # Lowercase
-    text = re.sub(r'[^\w\s]', '', text)  # Remove punctuation
-    return text.split()  # Tokenize
